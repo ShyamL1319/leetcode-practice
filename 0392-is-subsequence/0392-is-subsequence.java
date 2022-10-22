7c1b777a4aa963fa3cd75dac4a1d06be8b21202a
Shyam Lal <sshyamlalchauhan@gmail.com>
2022-10-23 02:28:38 +0530

    Time: 2 ms (66.14%), Space: 41.8 MB (70.39%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -1,19 +1,23 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
-        int maxLen = t.length();
-        int sLen = s.length();
-        if(sLen > maxLen) return false;
-        if(s.equals("")) return true;
-        int sPointer = 0;
-        int tPointer = 0;
-        while(tPointer < maxLen && sPointer < sLen){
-            char findChar = s.charAt(sPointer);
-            if(sPointer == sLen-1 && findChar == t.charAt(tPointer)) return true;
-            if(findChar == t.charAt(tPointer)){
-                sPointer++;
-            }
-            tPointer++;
-        }
-        return false;
+        // int maxLen = t.length();
+        // int sLen = s.length();
+        // if(sLen > maxLen) return false;
+        // if(s.equals("")) return true;
+        // int sPointer = 0;
+        // int tPointer = 0;
+        // while(tPointer < maxLen && sPointer < sLen){
+        //     char findChar = s.charAt(sPointer);
+        //     if(sPointer == sLen-1 && findChar == t.charAt(tPointer)) return true;
+        //     if(findChar == t.charAt(tPointer)){
+        //         sPointer++;
+        //     }
+        //     tPointer++;
+        // }
+        // return false;
+        int i = 0;
+        if(s.length() == 0) return true;
+        for(int j = 0; j < t.length(); j++) if(i < s.length() && s.charAt(i) == t.charAt(j)) i++;
+        return i == s.length();
     }
 }
