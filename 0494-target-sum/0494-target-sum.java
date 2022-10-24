e76901647a2fe49de39925d16f86a2766cc87fd5
Shyam Lal <sshyamlalchauhan@gmail.com>
2022-10-24 21:28:08 +0530

    Time: 225 ms (48.66%), Space: 42.7 MB (52.16%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,36 +1,55 @@
+// backtracking approach
 class Solution {
+    Map<Map.Entry<Integer, Integer>, Integer> map;
 
-    public int findTargetSumWays(int[] nums, int target) {
-        int sum = 0;
-        int n = nums.length;
-        for (int i = 0; i < n; i++) {
-            sum += nums[i];
-        }
-        if ((target + sum) % 2 != 0) return 0;
+    public int findTargetSumWays(int[] nums, int s) {
+        map = new HashMap<>();
+        return dp(nums, s, nums.length - 1, 0);
+    }
 
-        int m = (target + sum) / 2;
+    private int dp(int[] nums, int target, int index, int curSum) {
+        Map.Entry<Integer, Integer> entry = Map.entry(index, curSum);
+        if (map.containsKey(entry)) return map.get(entry);
 
-        if (m < 0) return 0;
+        if (index < 0 && curSum == target) return 1;
+        if (index < 0) return 0;
 
-        int dp[][] = new int[n + 1][m + 1];
+        int pos = dp(nums, target, index - 1, nums[index] + curSum);
+        int neg = dp(nums, target, index - 1, -nums[index] + curSum);
 
-        for (int i = 1; i <= m; i++) {
-            dp[0][i] = 0;
-        }
-        for (int i = 0; i <= n; i++) {
-            dp[i][0] = 1;
-        }
+        entry = Map.entry(index, curSum);
+        map.put(entry, pos + neg);
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 0; j <= m; j++) {
-                if (nums[i - 1] > j) {
-                    dp[i][j] = dp[i - 1][j];
-                } else {
-                    dp[i][j] = (dp[i - 1][j] + dp[i - 1][j - nums[i - 1]]);
-                }
-            }
-        }
-
-        return dp[n][m];
+        return pos + neg;
     }
 }
+// dp approach
+// class Solution {
+//     public int findTargetSumWays(int[] nums, int target) {
+//         int sum = 0;
+//         int n = nums.length;
+//         for (int i = 0; i < n; i++) {
+//             sum += nums[i];
+//         }
+//         if ((target + sum) % 2 != 0) return 0;
+//         int m = (target + sum) / 2;
+//         if (m < 0) return 0;
+//         int dp[][] = new int[n + 1][m + 1];
+//         for (int i = 1; i <= m; i++) {
+//             dp[0][i] = 0;
+//         }
+//         for (int i = 0; i <= n; i++) {
+//             dp[i][0] = 1;
+//         }
+//         for (int i = 1; i <= n; i++) {
+//             for (int j = 0; j <= m; j++) {
+//                 if (nums[i - 1] > j) {
+//                     dp[i][j] = dp[i - 1][j];
+//                 } else {
+//                     dp[i][j] = (dp[i - 1][j] + dp[i - 1][j - nums[i - 1]]);
+//                 }
+//             }
+//         }
+//         return dp[n][m];
+//     }
+// }
