0bad5c670a98cca48ab77879403b8b57fb8ad93d
Shyam Lal <sshyamlalchauhan@gmail.com>
2022-10-24 20:00:50 +0530

    Time: 4 ms (98.10%), Space: 41.5 MB (84.56%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,37 +1,36 @@
 class Solution {
+
     public int findTargetSumWays(int[] nums, int target) {
-        int sum=0;
+        int sum = 0;
         int n = nums.length;
-        for(int i =0; i<n;i++){
-            sum+=nums[i];
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
         }
-        if((target+sum) % 2 != 0) return 0;
-        
-        int m = (target+sum)/2;
-        
-        if(m < 0) return 0;
-        
-        int dp[][] = new int[n+1][m+1];
-        
-        for(int i=1; i<=m;i++){
-            dp[0][i]=0;
+        if ((target + sum) % 2 != 0) return 0;
+
+        int m = (target + sum) / 2;
+
+        if (m < 0) return 0;
+
+        int dp[][] = new int[n + 1][m + 1];
+
+        for (int i = 1; i <= m; i++) {
+            dp[0][i] = 0;
         }
-        for(int i=0; i<=n;i++){
+        for (int i = 0; i <= n; i++) {
             dp[i][0] = 1;
         }
-        
-        for(int i=1; i<=n; i++){
-            for(int j=0; j<=m; j++){
-                if(nums[i-1] > j){
-                    dp[i][j] = dp[i-1][j];
-                }else{
-                    dp[i][j] = (dp[i-1][j]+ dp[i-1][j-nums[i-1]]);
 
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= m; j++) {
+                if (nums[i - 1] > j) {
+                    dp[i][j] = dp[i - 1][j];
+                } else {
+                    dp[i][j] = (dp[i - 1][j] + dp[i - 1][j - nums[i - 1]]);
                 }
             }
         }
-        
+
         return dp[n][m];
-        
     }
 }
