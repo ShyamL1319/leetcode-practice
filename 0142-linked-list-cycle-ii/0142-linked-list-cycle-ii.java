3e1a82e8327ed4deb3778200ca61c5bf6a0c6930
Shyam Lal <sshyamlalchauhan@gmail.com>
2022-10-25 05:59:25 +0530

    Time: 2 ms (51.43%), Space: 45.2 MB (37.15%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -9,17 +9,38 @@
  *     }
  * }
  */
+// public class Solution {
+//     public ListNode detectCycle(ListNode head) {
+//         HashMap<String,Integer> hm = new HashMap<>();
+//         while( head != null && head.next != null){
+//             System.out.println(head);
+//             if(hm.getOrDefault(head+"",0) != 0){
+//                 return head;
+//             }else{
+//                 hm.put(head+"",1);
+//             }
+//             head= head.next;
+//         }
+//         return null;
+//     }
+// }
+// optimal approach
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-        HashMap<String,Integer> hm = new HashMap<>();
-        while( head != null && head.next != null){
-            System.out.println(head);
-            if(hm.getOrDefault(head+"",0) != 0){
-                return head;
-            }else{
-                hm.put(head+"",1);
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast){
+                slow = head;
+                while(slow != fast){
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+                return slow;
             }
-            head= head.next;
         }
         return null;
     }
