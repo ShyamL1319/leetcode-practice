4a7c9e06e30759a880a67b3e8a4904b2059c24c8
Shyam Lal <sshyamlalchauhan@gmail.com>
2022-10-27 23:31:18 +0530

    Time: 3 ms (96.86%), Space: 42.3 MB (91.31%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,24 +1,34 @@
 class Solution {
 
-    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        for (int i = 0; i < nums1.length; i++) {
-            boolean found = false;
-            int val = -1;
-            for (int j = 0; j < nums2.length; j++) {
-                if (nums2[j] == nums1[i]) {
-                    found = true;
-                    continue;
-                }
+    public int[] nextGreaterElement(int[] findNums, int[] nums) {
+//         for (int i = 0; i < nums1.length; i++) {
+//             boolean found = false;
+//             int val = -1;
+//             for (int j = 0; j < nums2.length; j++) {
+//                 if (nums2[j] == nums1[i]) {
+//                     found = true;
+//                     continue;
+//                 }
 
-                if (found && nums2[j] > nums1[i]) {
-                    val = nums2[j];
-                    break;
-                }
-            }
+//                 if (found && nums2[j] > nums1[i]) {
+//                     val = nums2[j];
+//                     break;
+//                 }
+//             }
 
-            nums1[i] = val;
-        }
+//             nums1[i] = val;
+//         }
 
-        return nums1;
+//         return nums1;
+        Map<Integer, Integer> map = new HashMap<>(); // map from x to next greater element of x
+        Stack<Integer> stack = new Stack<>();
+        for (int num : nums) {
+            while (!stack.isEmpty() && stack.peek() < num)
+                map.put(stack.pop(), num);
+            stack.push(num);
+        }   
+        for (int i = 0; i < findNums.length; i++)
+            findNums[i] = map.getOrDefault(findNums[i], -1);
+        return findNums;
     }
 }
