47ddb66514d84f1260773b936e7554ae2a4a0ba8
Shyam Lal <sshyamlalchauhan@gmail.com>
2022-11-01 02:42:44 +0530

    Time: 1 ms (78.22%), Space: 47.8 MB (47.35%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.java
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.java
@@ -1,15 +1,22 @@
 class Solution {
+
     public int diagonalSum(int[][] mat) {
-        int size = mat.length;
-        int sum=0;
-        if(size==0) return mat[0][0];
-        for(int row=0; row<size; row++){
-            sum += (mat[row][row] + mat[row][size-row-1]);
+        // int size = mat.length;
+        // int sum=0;
+        // if(size==0) return mat[0][0];
+        // for(int row=0; row<size; row++){
+        //     sum += (mat[row][row] + mat[row][size-row-1]);
+        // }
+        // //optimization little bit
+        // if(size%2 != 0){
+        //     sum-=mat[size/2][size/2];
+        // }
+        // return sum;
+        int sum = 0;
+        for (int i = 0; 2 * i < mat.length; i++) { //IMPROVEMENT IS THAT WE ONLY DO HALF ROW IN THE FOR LOOP
+            sum += mat[i][i] + mat[mat.length - i - 1][mat.length - i - 1] + mat[i][mat.length - i - 1] + mat[mat.length - i - 1][i];           //COUNT INTO OTHER CORRESPONDING THREE ELEMENT
         }
-        //optimization little bit
-        if(size%2 != 0){
-            sum-=mat[size/2][size/2];
-        }
-        return sum;
+
+        return mat.length % 2 == 1 ? sum -= mat[mat.length / 2][mat.length / 2] * 3 : sum;
     }
 }
