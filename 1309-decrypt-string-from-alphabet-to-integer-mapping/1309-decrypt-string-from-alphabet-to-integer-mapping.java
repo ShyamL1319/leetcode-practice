de9b069aa7faebdc2aba32c3cd980bc2a8510fca
Shyam Lal <sshyamlalchauhan@gmail.com>
2022-11-02 22:18:19 +0530

    Time: 22 ms (5.29%), Space: 42.3 MB (50.77%) - LeetHub

---
+++ 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.java
@@ -0,0 +1,27 @@
+class Solution {
+
+    public String freqAlphabets(String s) {
+        StringBuffer sb = new StringBuffer();
+        int n = s.length();
+        int pos = n - 1; //last
+        while (pos >= 0) {
+            if (s.charAt(pos) == '#') {
+                int a = (int) (s.charAt(pos - 2) - '0');
+                int b = (int) (s.charAt(pos - 1) - '0');
+                int numb = a * 10 + b;
+                char generate = (char) ('a' + numb - 1);
+                sb.append(generate);
+                pos = pos - 3;
+                System.out.println(numb);
+            } else {
+                int numb = (int) (s.charAt(pos) - '0');
+                char generate = (char) ('a' + numb - 1);
+                sb.append(generate);
+                pos = pos - 1;
+                System.out.println(numb);
+            }
+        }
+
+        return sb.reverse().toString();
+    }
+}
