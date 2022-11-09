0cc7b0561f283aec3d082489f2258131f23cabe6
Shyam Lal <sshyamlalchauhan@gmail.com>
2022-11-09 23:23:40 +0530

    Time: 42 ms (5.64%), Space: 49 MB (7.76%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,54 +1,63 @@
 class Solution {
+
     public boolean isValidSudoku(char[][] board) {
-        HashSet<Character> rowSet = new HashSet<>();
-        HashSet<Character> colSet = new HashSet<>();
-        int flag = 1;
-        //check all row has any duplicate
-        for(int i=0; i<9; i++){
-            colSet = new HashSet<>();
-            rowSet = new HashSet<>();
-            for(int j=0; j<9; j++){
-                if(colSet.contains(board[i][j]) || rowSet.contains(board[j][i])){
-                    flag=0;
-                    break;
-                }else{
-                    if(board[i][j] != '.')
-                        colSet.add(board[i][j]);
-                    if(board[j][i] != '.')
-                        rowSet.add(board[j][i]);
+        Set seen = new HashSet();
+        for (int i = 0; i < 9; ++i) {
+            for (int j = 0; j < 9; ++j) {
+                if (board[i][j] != '.') {
+                    String b = "(" + board[i][j] + ")";
+                    if (!seen.add(b + i) || !seen.add(j + b) || !seen.add(i / 3 + b + j / 3)) return false;
                 }
             }
-            if(flag==0) break;
         }
-        
-        if(flag==0) return false;
-        
-        // check 3*3
-        return (check3X3(board,0) && check3X3(board,3) && check3X3(board,6));
+        return true;
+
+        //Brute force 
+//         HashSet<Character> rowSet = new HashSet<>();
+//         HashSet<Character> colSet = new HashSet<>();
+//         int flag = 1;
+//         //check all row has any duplicate
+//         for (int i = 0; i < 9; i++) {
+//             colSet = new HashSet<>();
+//             rowSet = new HashSet<>();
+//             for (int j = 0; j < 9; j++) {
+//                 if (colSet.contains(board[i][j]) || rowSet.contains(board[j][i])) {
+//                     flag = 0;
+//                     break;
+//                 } else {
+//                     if (board[i][j] != '.') colSet.add(board[i][j]);
+//                     if (board[j][i] != '.') rowSet.add(board[j][i]);
+//                 }
+//             }
+//             if (flag == 0) break;
+//         }
+
+//         if (flag == 0) return false;
+
+//         // check 3*3
+//         return (check3X3(board, 0) && check3X3(board, 3) && check3X3(board, 6));
     }
-    
-    public boolean check3X3(char[][] board, int col){
+
+    public boolean check3X3(char[][] board, int col) {
         int k = 0;
         HashSet<Character> set = new HashSet<>();
         int flag = 1;
-        while(k<9){
+        while (k < 9) {
             set = new HashSet<>();
-            for(int i=0; i<3; i++){
-                for(int j=0; j<3; j++){
-                    if(set.contains(board[i+k][j+col])){
-                        flag=0;
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    if (set.contains(board[i + k][j + col])) {
+                        flag = 0;
                         break;
-                    }else{
-                        if(board[i+k][j+col] != '.')
-                            set.add(board[i+k][j+col]);
+                    } else {
+                        if (board[i + k][j + col] != '.') set.add(board[i + k][j + col]);
                     }
                 }
-                if(flag==0) break;
+                if (flag == 0) break;
             }
-            k+=3;
-            if(flag==0) break;
+            k += 3;
+            if (flag == 0) break;
         }
-        if(flag == 0) return false;
-        else return true;
+        if (flag == 0) return false; else return true;
     }
 }
