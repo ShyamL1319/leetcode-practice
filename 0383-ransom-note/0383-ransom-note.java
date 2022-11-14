8ed8d181fc2c4e30b9f09185cc29ee7fc3cc9e8d
Shyam Lal <sshyamlalchauhan@gmail.com>
2022-11-14 21:20:51 +0530

    Time: 2 ms (99.29%), Space: 42.4 MB (96.68%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,24 +1,28 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
         
-        HashMap<Character,Integer> hmap = new HashMap<>();
+        //HashMap<Character,Integer> hmap = new HashMap<>();
+        int charA [] = new int[26];
         for(int i=0; i<magazine.length(); i++){
             char ch = magazine.charAt(i);
-            if(hmap.containsKey(ch)){
-                hmap.put(ch, hmap.get(ch)+1);
-            }else{
-                hmap.put(ch,1);
-            }
+            // if(hmap.containsKey(ch)){
+            //     hmap.put(ch, hmap.get(ch)+1);
+            // }else{
+            //     hmap.put(ch,1);
+            // }
+            charA[ch-'a']+=1;
         }
         
         for(int i =0; i<ransomNote.length(); i++){
             char ch = ransomNote.charAt(i);
-            if(!hmap.containsKey(ransomNote.charAt(i))) return false;
-            else{
-              int cnt =  (int)hmap.get(ch);
-                if(cnt <= 0) return false;
-                else hmap.put(ch, cnt-1);
-            }
+            // if(!hmap.containsKey(ransomNote.charAt(i))) return false;
+            // else{
+            //   int cnt =  (int)hmap.get(ch);
+            //     if(cnt <= 0) return false;
+            //     else hmap.put(ch, cnt-1);
+            // }
+            if(charA[ch-'a'] <= 0) return false;
+            else charA[ch-'a'] -= 1;
         }
         return true;
     }
