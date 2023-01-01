d00ed5a3f0bf3a3ed5d2f25151ac9f88dcfb4222
Shyam Lal <sshyamlalchauhan@gmail.com>
2023-01-02 02:51:16 +0530

    Time: 1 ms (95.21%), Space: 40.1 MB (89.81%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.java
+++ 0290-word-pattern/0290-word-pattern.java
@@ -1,22 +1,32 @@
 class Solution {
 
     public boolean wordPattern(String pattern, String s) {
-        char patternArr[] = pattern.toCharArray();
-        String strArr[] = s.split(" ");
+        //         char patternArr[] = pattern.toCharArray();
+        //         String strArr[] = s.split(" ");
 
-        if (patternArr.length != strArr.length) return false;
+        //         if (patternArr.length != strArr.length) return false;
+        //         HashMap<Character, String> map = new HashMap<>();
+        //         HashMap<String, Character> viceMap = new HashMap<>();
+        //         for (int i = 0; i < patternArr.length; i++) {
+        //             if (map.containsKey(patternArr[i]) && viceMap.containsKey(strArr[i])) {
+        //                 if (!map.get(patternArr[i]).equals(strArr[i]) || !viceMap.get(strArr[i]).equals(patternArr[i])) {
+        //                     return false;
+        //                 }
+        //             } else {
+        //                 if (!(map.getOrDefault(patternArr[i], " ").equals(" ") && viceMap.getOrDefault(strArr[i], ' ') == ' ')) return false;
+        //                 map.put(patternArr[i], strArr[i]);
+        //                 viceMap.put(strArr[i], patternArr[i]);
+        //             }
+        //         }
+        //         return true;
+        String[] arr = s.split(" ");
+        if (pattern.length() != arr.length) return false;
         HashMap<Character, String> map = new HashMap<>();
-        HashMap<String, Character> viceMap = new HashMap<>();
-        for (int i = 0; i < patternArr.length; i++) {
-            if (map.containsKey(patternArr[i]) && viceMap.containsKey(strArr[i])) {
-                if (!map.get(patternArr[i]).equals(strArr[i]) || !viceMap.get(strArr[i]).equals(patternArr[i])) {
-                    return false;
-                }
-            } else {
-                if (!(map.getOrDefault(patternArr[i], " ").equals(" ") && viceMap.getOrDefault(strArr[i], ' ') == ' ')) return false;
-                map.put(patternArr[i], strArr[i]);
-                viceMap.put(strArr[i], patternArr[i]);
-            }
+        for (int i = 0; i < pattern.length(); i++) {
+            char ch = pattern.charAt(i);
+            boolean containsKey = map.containsKey(ch);
+            if (map.containsValue(arr[i]) && !containsKey) return false;
+            if (containsKey && !map.get(ch).equals(arr[i])) return false; else map.put(ch, arr[i]);
         }
         return true;
     }
